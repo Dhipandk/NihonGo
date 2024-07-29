c9d21792750e8c1a8c692ec4f63e206683a10db7
unknown <DhipanDk>
2024-07-29 06:22:10 +0530

    mic check

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -171,6 +171,247 @@
 // export default SendMessage;
 
 
+// import React, { useState, useEffect } from "react";
+// import { auth, db } from "./Firebase";
+// import { addDoc, collection, serverTimestamp, doc, updateDoc } from "firebase/firestore";
+// import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
+// import debounce from "lodash.debounce";
+// // import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// const SendMessage = ({ scroll, currentRoom, preferences }) => {
+//   const [message, setMessage] = useState("");
+//   const [translatedMessage, setTranslatedMessage] = useState("");
+//   const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+//   // const [isTranslating,setIsTranslating] = useState(false);
+//   const { uid, displayName, photoURL } = auth.currentUser;
+//   const [preferredLanguage, setPreferredLanguage] = useState(preferences.preferredLanguage || "en"); // Default to English if no preference
+//   const [translatedLanguage, setTranslatedLanguage] = useState(preferredLanguage === "en" ? "ja" : "en"); 
+//   // const prefer = preferredLanguage;
+//   // const trans = translatedLanguage;
+
+//   const updateTypingStatus = async (isTyping, currentMessage = "", currentTranslation = "") => {
+//     if (auth.currentUser) {
+//       const userRef = doc(db, "users", auth.currentUser.uid);
+//       await updateDoc(userRef, {
+//         typing: isTyping,
+//         currentMessage: currentMessage,
+//         translatedMessage: currentTranslation,
+//         roomId: currentRoom.id,
+//         uid:uid,
+//         displayName:displayName,
+//         photoURL:photoURL,
+//       });
+//     }
+//   };
+//   useEffect(() => {
+//     setPreferredLanguage(preferences.preferredLanguage);
+//     setTranslatedLanguage(preferences.preferredLanguage === "en" ? "ja" : "en");
+//   }, [preferences]);
+
+//   useEffect(() => {
+//     const delayDebounceFn = setTimeout(async () => {
+//       if (message.trim() === "") {
+//         setTranslatedMessage("");
+//         await updateTypingStatus(false);
+//         return;
+//       }
+
+//       const translatedText = await detectLanguageAndTranslate(message);
+//       setTranslatedMessage(translatedText);
+//       await updateTypingStatus(true, message, translatedText);
+//     }, 300); // Debounce time set to 300ms
+
+//     return () => clearTimeout(delayDebounceFn);
+//   }, [message]);
+
+//   const detectLanguageAndTranslate = async (text) => {
+//     try {
+//       // const isAscii = preferences.preferredLanguage;
+//       let fromLang = preferredLanguage;
+//       let toLang = translatedLanguage;
+
+//       const translatedText = await translateText(text, fromLang, toLang);
+//       return translatedText;
+//     } catch (error) {
+//       console.error("Error detecting language and translating:", error);
+//       return "Translation Error";
+//     }
+//   };
+
+//   const translateText = async (text, from, to) => {
+//     if (!text.trim()) return "";
+//     const authKey = "8d076432-892a-4c29-8854-890321883b5c:fx";
+
+// //   try {
+// //     const response = await fetch(
+// //       `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=${from}|${to}`
+// //     );
+
+// //     const data = await response.json();
+// //     return data.responseData.translatedText;
+// //   } catch (error) {
+// //     console.error("Error translating:", error);
+// //     return "Translation Error";
+// //   }
+// // };
+
+//     try {
+//       const response = await fetch("https://api-free.deepl.com/v2/translate", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: new URLSearchParams({
+//           auth_key: authKey,
+//           text: text,
+//           source_lang: from,
+//           target_lang: to,
+//         }),
+//       });
+
+//       const data = await response.json();
+//       return data.translations[0].text;
+//     } catch (error) {
+//       console.error("Error translating:", error);
+//       return "Translation Error";
+//     }
+// // const genAI = new GoogleGenerativeAI("AIzaSyBWA_LHGQqKhEMKY20OqS6qSvpuKmDnHP4");
+
+// // try {
+
+// //   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+// //   const prompt = `Translate the following text from ${from} to ${to}: ${text}`;
+
+// //   const result = await model.generateContent(prompt);
+// //   const response = result.response;
+// //   const translatedText = await response.text();
+// //   return translatedText;
+// // } catch (error) {
+// //   console.error("Error translating:", error);
+// //   return "Translation Error";
+// // }
+
+   
+//   };
+
+
+//   const debouncedTranslate = debounce(async (text) => {
+//     const translatedText = await detectLanguageAndTranslate(text);
+//     setTranslatedMessage(translatedText);
+//     await updateTypingStatus(true, text, translatedText);
+//   }, 300); // Debounce time set to 300ms
+
+//   useEffect(() => {
+//     if (message.trim() === "") {
+//       setTranslatedMessage("");
+//       updateTypingStatus(false);
+//       return;
+//     }
+//     debouncedTranslate(message);
+//   }, [message]);
+
+//   const handleSendMessage = async (text) => {
+//     if (text.trim() === "") {
+//       alert("Enter valid message");
+//       return;
+//     }
+
+//     try {
+//       const translatedText = await detectLanguageAndTranslate(text);
+//       setTranslatedMessage(translatedText);
+
+//       await addDoc(collection(db, "messages"), {
+//         text: text,
+//         name: displayName,
+//         avatar: photoURL,
+//         createdAt: serverTimestamp(),
+//         uid,
+//         roomId: currentRoom.id,
+//         translatedtext: translatedText,
+//       });
+
+//       setMessage("");
+//       await updateTypingStatus(false);
+//       scroll.current.scrollIntoView({ behavior: "smooth" });
+//     } catch (error) {
+//       console.error("Error sending message:", error);
+//     }
+//   };
+
+//   const handleMicClick = async () => {
+//     const preferLanguage = preferredLanguage; // Default to English if no preference
+
+//     try {
+//       if (listening) {
+//         SpeechRecognition.stopListening();
+//         await handleSendMessage(transcript);
+//         resetTranscript();
+//       } else {
+//         await navigator.mediaDevices.getUserMedia({ audio: true });
+//         SpeechRecognition.startListening({ continuous: true, language: preferLanguage });
+//       }
+//     } catch (error) {
+//       console.error("Error with speech recognition:", error);
+//     }
+//   };
+
+//   const handleInputKeyDown = (e) => {
+//     if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
+//       e.preventDefault(); // Prevent default behavior (form submission)
+//       handleSendMessage(message);
+//     }
+//   };
+//   const handleLanguageSwitch = async () => {
+//     // Use a temporary variable to swap the languages
+//     const tempLanguage = preferredLanguage;
+//     setPreferredLanguage(translatedLanguage);
+//     setTranslatedLanguage(tempLanguage);
+
+//     // const userRef = doc(db, "users", auth.currentUser.uid);
+//     // await updateDoc(userRef, {
+//     //   preferredLanguage: prefer,
+//     //   translatedLanguage: trans,
+//     // });
+
+//   };
+
+
+
+//   return (
+//     <div>
+//       {!browserSupportsSpeechRecognition && <span>Browser doesn't support speech recognition.</span>}
+//       <form onSubmit={(e) => { e.preventDefault(); handleSendMessage(message); }} className="send-message">
+//         <textarea
+//           id="messageInput"
+//           name="messageInput"
+//           type="text"
+//           className="form-input__input"
+//           placeholder="Type message..."
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//           onKeyDown={handleInputKeyDown}
+//           onBlur={() => updateTypingStatus(false)}
+//         />
+        
+//         <button type="submit" className="send">Send</button>
+//         <button type="button" onClick={handleMicClick} className="mic-icon">
+//           <FontAwesomeIcon icon={listening ? faMicrophone : faMicrophoneSlash}  />
+//         </button>
+//         <button type="button" onClick={handleLanguageSwitch} className="switch-icon">
+//         <div>
+//           {preferredLanguage === "en" ? "EN" : "JP"}
+//         </div>
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SendMessage;
+
+
 import React, { useState, useEffect } from "react";
 import { auth, db } from "./Firebase";
 import { addDoc, collection, serverTimestamp, doc, updateDoc } from "firebase/firestore";
@@ -178,18 +419,14 @@ import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognitio
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 import debounce from "lodash.debounce";
-// import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const SendMessage = ({ scroll, currentRoom, preferences }) => {
   const [message, setMessage] = useState("");
   const [translatedMessage, setTranslatedMessage] = useState("");
   const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
-  // const [isTranslating,setIsTranslating] = useState(false);
   const { uid, displayName, photoURL } = auth.currentUser;
-  const [preferredLanguage, setPreferredLanguage] = useState(preferences.preferredLanguage || "en"); // Default to English if no preference
-  const [translatedLanguage, setTranslatedLanguage] = useState(preferredLanguage === "en" ? "ja" : "en"); 
-  // const prefer = preferredLanguage;
-  // const trans = translatedLanguage;
+  const [preferredLanguage, setPreferredLanguage] = useState(preferences.preferredLanguage || "en");
+  const [translatedLanguage, setTranslatedLanguage] = useState(preferences.preferredLanguage === "en" ? "ja" : "en");
 
   const updateTypingStatus = async (isTyping, currentMessage = "", currentTranslation = "") => {
     if (auth.currentUser) {
@@ -199,12 +436,13 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
         currentMessage: currentMessage,
         translatedMessage: currentTranslation,
         roomId: currentRoom.id,
-        uid:uid,
-        displayName:displayName,
-        photoURL:photoURL,
+        uid,
+        displayName,
+        photoURL,
       });
     }
   };
+
   useEffect(() => {
     setPreferredLanguage(preferences.preferredLanguage);
     setTranslatedLanguage(preferences.preferredLanguage === "en" ? "ja" : "en");
@@ -221,14 +459,13 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
       const translatedText = await detectLanguageAndTranslate(message);
       setTranslatedMessage(translatedText);
       await updateTypingStatus(true, message, translatedText);
-    }, 300); // Debounce time set to 300ms
+    }, 300);
 
     return () => clearTimeout(delayDebounceFn);
   }, [message]);
 
   const detectLanguageAndTranslate = async (text) => {
     try {
-      // const isAscii = preferences.preferredLanguage;
       let fromLang = preferredLanguage;
       let toLang = translatedLanguage;
 
@@ -243,19 +480,6 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
   const translateText = async (text, from, to) => {
     if (!text.trim()) return "";
     const authKey = "8d076432-892a-4c29-8854-890321883b5c:fx";
-
-//   try {
-//     const response = await fetch(
-//       `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=${from}|${to}`
-//     );
-
-//     const data = await response.json();
-//     return data.responseData.translatedText;
-//   } catch (error) {
-//     console.error("Error translating:", error);
-//     return "Translation Error";
-//   }
-// };
 
     try {
       const response = await fetch("https://api-free.deepl.com/v2/translate", {
@@ -277,31 +501,13 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
       console.error("Error translating:", error);
       return "Translation Error";
     }
-// const genAI = new GoogleGenerativeAI("AIzaSyBWA_LHGQqKhEMKY20OqS6qSvpuKmDnHP4");
-
-// try {
-
-//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-//   const prompt = `Translate the following text from ${from} to ${to}: ${text}`;
-
-//   const result = await model.generateContent(prompt);
-//   const response = result.response;
-//   const translatedText = await response.text();
-//   return translatedText;
-// } catch (error) {
-//   console.error("Error translating:", error);
-//   return "Translation Error";
-// }
-
-   
   };
-
 
   const debouncedTranslate = debounce(async (text) => {
     const translatedText = await detectLanguageAndTranslate(text);
     setTranslatedMessage(translatedText);
     await updateTypingStatus(true, text, translatedText);
-  }, 300); // Debounce time set to 300ms
+  }, 300);
 
   useEffect(() => {
     if (message.trim() === "") {
@@ -341,43 +547,34 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
   };
 
   const handleMicClick = async () => {
-    const preferLanguage = preferredLanguage; // Default to English if no preference
-
     try {
+      const preferLanguage = preferredLanguage;
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       if (listening) {
         SpeechRecognition.stopListening();
         await handleSendMessage(transcript);
         resetTranscript();
       } else {
-        await navigator.mediaDevices.getUserMedia({ audio: true });
         SpeechRecognition.startListening({ continuous: true, language: preferLanguage });
       }
     } catch (error) {
       console.error("Error with speech recognition:", error);
+      alert("Please allow microphone access for using this feature.");
     }
   };
 
   const handleInputKeyDown = (e) => {
     if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
-      e.preventDefault(); // Prevent default behavior (form submission)
+      e.preventDefault();
       handleSendMessage(message);
     }
   };
+
   const handleLanguageSwitch = async () => {
-    // Use a temporary variable to swap the languages
     const tempLanguage = preferredLanguage;
     setPreferredLanguage(translatedLanguage);
     setTranslatedLanguage(tempLanguage);
-
-    // const userRef = doc(db, "users", auth.currentUser.uid);
-    // await updateDoc(userRef, {
-    //   preferredLanguage: prefer,
-    //   translatedLanguage: trans,
-    // });
-
   };
-
-
 
   return (
     <div>
@@ -394,15 +591,14 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
           onKeyDown={handleInputKeyDown}
           onBlur={() => updateTypingStatus(false)}
         />
-        
         <button type="submit" className="send">Send</button>
         <button type="button" onClick={handleMicClick} className="mic-icon">
-          <FontAwesomeIcon icon={listening ? faMicrophone : faMicrophoneSlash}  />
+          <FontAwesomeIcon icon={listening ? faMicrophone : faMicrophoneSlash} />
         </button>
         <button type="button" onClick={handleLanguageSwitch} className="switch-icon">
-        <div>
-          {preferredLanguage === "en" ? "EN" : "JP"}
-        </div>
+          <div>
+            {preferredLanguage === "en" ? "EN" : "JP"}
+          </div>
         </button>
       </form>
     </div>
@@ -410,5 +606,3 @@ const SendMessage = ({ scroll, currentRoom, preferences }) => {
 };
 
 export default SendMessage;
-
-
